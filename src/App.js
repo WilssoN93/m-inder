2371255eb6810d6d065c85d6c397ab422f260e65
WilssoN93 <wilssondaniel@gmail.com>
2021-03-10 20:25:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Sidebar from "./Sidebar";
+import Movies from "./Movies";
+import { auth } from "./firebase";
+import Login from "./Login";
+import LoadingContainer from "./LoadingContainer";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <LoadingContainer />;
+  }
+
+  return user != null ? (
+    <div className="app">
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Movies />
+      </div>
     </div>
+  ) : (
+    <Login />
   );
 }
 
